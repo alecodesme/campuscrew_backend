0053bdc3a3e4301a1f68429118cf7a4127104321
Alejandro Gonzalez <alejandrogonzalez@MacBook-Pro-de-Alejandro.local>
2024-12-05 13:32:05 +0100

    template email accepted done

--- resources/views/emails/acceptUniversity.blade.php
+++ resources/views/emails/acceptUniversity.blade.php
@@ -6,15 +6,15 @@
     <title>University Acceptance</title>
 </head>
 
-<body style="font-family: Arial, sans-serif; background-color: #f3f4f6; margin: 0; padding: 0;">
+<body style="font-family: Arial, sans-serif; background-color: #FFFFF; margin: 0; padding: 0;">
 
     <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 8px; overflow: hidden; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
-        <!-- Header -->
-        <div style="background-color: #3b82f6; padding: 20px; text-align: center;">
-            <h1 style="color: #ffffff; font-size: 24px; margin: 0;">Congratulations!</h1>
+        <div style="background-color: #ECEBE4; padding: 20px; text-align: center;">
+
+            <img height="100" width="78" src="https://iili.io/2ESGjtt.png" />
+            <h1 style="color: black; font-size: 24px; margin: 0;">Congratulations!</h1>
         </div>
 
-        <!-- Content -->
         <div style="padding: 20px;">
             <p style="font-size: 18px; color: #374151; margin-bottom: 20px;">
                 Dear Applicant,
@@ -25,7 +25,7 @@
             <p style="font-size: 16px; color: #4b5563; margin-bottom: 20px;">
                 Please check your email for further instructions on how to proceed. We look forward to seeing you thrive in your academic journey.
             </p>
-            <a href="http://localhost:8080/auth/sign-in" style="display: inline-block; background-color: #3b82f6; color: #ffffff; text-decoration: none; padding: 12px 24px; border-radius: 4px; font-size: 16px; text-align: center;">
+            <a href="http://localhost:8080/auth/sign-in" style="display: inline-block; background-color: black; color: #ffffff; text-decoration: none; padding: 12px 24px; border-radius: 4px; font-size: 16px; text-align: center;">
                 Access Your Dashboard
             </a>
 
